138784a3c4e133b171f8f88fc26c81a5ccb1c03c
solihFrontDEV <solihmirhalilov8@gmail.com>
2025-07-23 14:22:39 +0500

    app.jsx

---
+++ frontend/src/Pages/Navbar.jsx
@@ -0,0 +1,17 @@
+import Uzb from "../assets/uzbekistan.png"
+export default function Navbar() {
+  return (
+    <>
+      <nav>
+        <div>
+          {/* Nanbar Mini */}
+          <div>
+            <div>
+                <div><img src="" alt="" /></div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
